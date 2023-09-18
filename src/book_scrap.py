ac21c5e0de97f9294cbcc886e162ba39a250c573
Jérôme Peyras <jerome.peyras@protonmail.com>
2023-09-18 14:06:32 +0200

    Moves scrapping function to own module

---
+++ src/book_scrap.py
@@ -0,0 +1,60 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrap_a_book(url: str) -> dict[str, str]:
+    """A function that takes a book url as parameter and
+    returns a dictionary of the book's data"""
+    book_infos = {
+        "product_page_url": url,
+        "upc": "",
+        "title": "",
+        "price_including_tax": "",
+        "price_excluding_tax": "",
+        "number_available": "",
+        "product_description": "",
+        "category": "",
+        "review_rating": "",
+        "image_url": "",
+    }
+    rating_map = {
+        "One": "1",
+        "Two": "2",
+        "Three": "3",
+        "Four": "4",
+        "Five": "5",
+    }
+    r = requests.get(url)
+    if r.ok:
+        soup = BeautifulSoup(r.text, features="html.parser")
+        table = soup.find_all("td")
+        breadcrumbs = list(
+            filter(
+                lambda i: i != "\n",
+                soup.find("ul", class_="breadcrumb").contents,
+            )
+        )
+        category = breadcrumbs[2].text.strip()
+        rating = soup.find("p", class_="star-rating")
+        image_source = list(
+            filter(
+                lambda i: i != "\n",
+                soup.find("div", class_="item active").contents,
+            )
+        )
+
+        book_infos["upc"] = table[0].text
+        book_infos["title"] = soup.find("h1").text
+        book_infos["price_including_tax"] = table[3].text
+        book_infos["price_excluding_tax"] = table[2].text
+        book_infos["number_available"] = re.findall(r"\d+", table[5].text)[0]
+        book_infos["product_description"] = (
+            soup.find("div", id="product_description").find_next("p").text
+        )
+        book_infos["category"] = category
+        book_infos["review_rating"] = rating_map[rating["class"][1]]
+        book_infos["image_url"] = (
+            "https://books.toscrape.com/" + image_source[0]["src"][6:]
+        )
+    return book_infos
--- src/main.py
+++ src/main.py
@@ -1,52 +1,13 @@
 import csv
-import re
-import requests
-from bs4 import BeautifulSoup
+from book_scrap import scrap_a_book
 
-url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-book_infos = {
-    "product_page_url": url,
-    "upc": "",
-    "title": "",
-    "price_including_tax": "",
-    "price_excluding_tax": "",
-    "number_available": "",
-    "product_description": "",
-    "category": "",
-    "review_rating": "",
-    "image_url": "",
-}
+url = "https://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html"
 
-rating_map = {"One": "1", "Two": "2", "Three": "3", "Four": "4", "Five": "5"}
+book_data = scrap_a_book(url=url)
 
-r = requests.get(url)
-if r.ok:
-    soup = BeautifulSoup(r.text, features="html.parser")
-    table = soup.find_all("td")
-    breadcrumbs = list(
-        filter(lambda i: i != "\n", soup.find("ul", class_="breadcrumb").contents)
-    )
-    category = breadcrumbs[2].text.strip()
-    rating = soup.find("p", class_="star-rating")
-    image_source = list(
-        filter(lambda i: i != "\n", soup.find("div", class_="item active").contents)
-    )
+with open("books-data/category.csv", "w", newline="") as csvfile:
+    fieldnames = list(book_data)
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    book_infos["upc"] = table[0].text
-    book_infos["title"] = soup.find("h1").text
-    book_infos["price_including_tax"] = table[3].text
-    book_infos["price_excluding_tax"] = table[2].text
-    book_infos["number_available"] = re.findall(r"\d+", table[5].text)[0]
-    book_infos["product_description"] = (
-        soup.find("div", id="product_description").find_next("p").text
-    )
-    book_infos["category"] = category
-    book_infos["review_rating"] = rating_map[rating["class"][1]]
-    book_infos["image_url"] = "https://books.toscrape.com/" + image_source[0]["src"][6:]
-
-    with open("books-data/single_book.csv", "w", newline="") as csvfile:
-        fieldnames = list(book_infos)
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
-        writer.writerow(book_infos)
+    writer.writeheader()
+    writer.writerow(book_data)
