232836ee92ada0538730003b1098ed6588503aa7
Jérôme Peyras <jerome.peyras@protonmail.com>
2023-09-18 16:12:09 +0200

    Adds pagination handling

--- src/category_scrap.py
+++ src/category_scrap.py
@@ -1,10 +1,11 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 
 
 def get_books_urls(url: str) -> list[str]:
     r = requests.get(url)
-    products = []
+    products: list[str] = []
 
     if r.ok:
         soup = BeautifulSoup(r.text, features="html.parser")
@@ -12,3 +13,15 @@ def get_books_urls(url: str) -> list[str]:
         products = [p.h3.a["href"][9:] for p in product_pods]
 
     return products
+
+
+def get_category_page_number(url: str) -> int:
+    r = requests.get(url)
+    if r.ok:
+        soup = BeautifulSoup(r.text, features="html.parser")
+        pager = soup.find("li", class_="current")
+        if pager:
+            max_page = re.findall(r"\d+", pager.text.strip())[1]
+            return int(max_page)
+        return 1
+    return 0
--- src/main.py
+++ src/main.py
@@ -1,17 +1,30 @@
 import csv
 from book_scrap import scrap_a_book
-from category_scrap import get_books_urls
+from category_scrap import get_books_urls, get_category_page_number
 
-category_url = (
-    "https://books.toscrape.com/catalogue/category/books/travel_2/index.html"
-)
+category_url = "https://books.toscrape.com/catalogue/category/books/mystery_3"
 
-books_urls = get_books_urls(url=category_url)
+page_number: int = get_category_page_number(url=category_url)
+
 books = []
+if page_number > 1:
+    for i in range(page_number):
+        page_url = f"{category_url}/page-{i + 1}.html"
+        books_urls = get_books_urls(url=page_url)
 
-for link in books_urls:
-    book = scrap_a_book(url="https://books.toscrape.com/catalogue/" + link)
-    books.append(book)
+        for link in books_urls:
+            book = scrap_a_book(
+                url="https://books.toscrape.com/catalogue/" + link
+            )
+            books.append(book)
+elif page_number == 1:
+    books_urls = get_books_urls(url=category_url + "/index.html")
+
+    for link in books_urls:
+        book = scrap_a_book(url="https://books.toscrape.com/catalogue/" + link)
+        books.append(book)
+else:
+    print("Invalid url")
 
 with open("books-data/category.csv", "w", newline="") as csvfile:
     fieldnames = list(books[0])
